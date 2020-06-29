c95423925e524a0a9e60a62bc9f1741901d2408c
Jordan Perkins <jordanperkins@hotmail.co.uk>
2020-06-29 11:16:28 +0100

    added localStorage

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const newBookButton = document.querySelector("#new-book");
 const closeIcon = document.querySelector(".close-icon");
 
 
-
 const submit = document.querySelector("#add");
 
 submit.addEventListener("click",(e) =>{
@@ -28,6 +27,7 @@ newBookButton.addEventListener("click", (e) =>{
     overlay.style.display = "block";
 })
 
+getArray();
 
 function Book(title, author, pages, read){
     this.title = title
@@ -44,11 +44,10 @@ function addBookToLibrary(title, author, pages, read){
     const newBook = new Book(title, author, pages, read);
     newBook.prototype = Object.create(Book.prototype);
     myLibrary.push(newBook);
+    setArray();
 }
 
-function render(){
-    CreateCard();
-}
+
 
 function resetForm(form){
     form.title.value = "";
@@ -56,31 +55,31 @@ function resetForm(form){
     form.pages.value = "";
 }
 
-function CreateCard() {
+function CreateCard(index=myLibrary.length - 1) {
     
     //Creating the card 
     const card = document.createElement("div");
     card.setAttribute("id", "card");
-    card.setAttribute("data",myLibrary.length - 1);
+    card.setAttribute("data",index);
     //Creating the heading (title of book)
     const heading = document.createElement("h3");
-    heading.textContent = myLibrary[myLibrary.length-1].title;
+    heading.textContent = myLibrary[index].title;
     heading.setAttribute("class", "title");
     card.appendChild(heading);
 
     //Creating and adding author to the card
     const author = document.createElement("p");
-    author.textContent = `by ${myLibrary[myLibrary.length-1].author}`;
+    author.textContent = `by ${myLibrary[index].author}`;
     author.setAttribute("class", "info");
     card.appendChild(author);
 
     const pages = document.createElement("p");
-    pages.textContent = `${myLibrary[myLibrary.length-1].pages} pages`;
+    pages.textContent = `${myLibrary[index].pages} pages`;
     pages.setAttribute("class", "info");
     card.appendChild(pages);
 
     const read = document.createElement("p");
-    read.textContent = `Read: ${myLibrary[myLibrary.length-1].read}`;
+    read.textContent = `Read: ${myLibrary[index].read}`;
     read.setAttribute("class", "info");
     card.appendChild(read);
 
@@ -96,11 +95,11 @@ function CreateCard() {
     const changeRead = document.createElement("button");
     changeRead.setAttribute("id","change-read");
     changeRead.addEventListener("click", ()=>{
-        if(myLibrary[myLibrary.length-1].read === "Yes"){
-            myLibrary[myLibrary.length-1].read = "No";
+        if(myLibrary[index].read === "Yes"){
+            myLibrary[index].read = "No";
             changeRead.parentElement.children[3].textContent = "Read: No"
         } else{
-            myLibrary[myLibrary.length-1].read = "Yes";
+            myLibrary[index].read = "Yes";
             changeRead.parentElement.children[3].textContent = "Read: Yes"
         }
     });
@@ -112,13 +111,56 @@ function CreateCard() {
     
 }
 
-//Delete once done
-addBookToLibrary("Harry Potter", "J.K Rowling", 245, "Yes");
-render()
+function storageAvailable(type) {
+    var storage;
+    try {
+        storage = window[type];
+        var x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
 
-const removeBook = document.querySelector("#remove-book");
-removeBook.addEventListener("click", (e)=>{
-    myLibrary.splice(removeBook.parentElement.getAttribute("data"),1);
-    e.srcElement.parentElement.remove();
+function setArray(){
+    if (storageAvailable("localStorage")){
+        localStorage.setItem("library", JSON.stringify(myLibrary));
+    }
+    else{ return; }
     
-})
+}
+
+function getArray(){
+    if(localStorage.getItem('library')){
+        myLibrary = JSON.parse(localStorage.getItem("library"));
+        render();
+    }
+    else{ return; }
+}
+
+function render(){
+    let cardCount = document.querySelector("#books").childElementCount;
+    if(cardCount - myLibrary.length < -1){
+        for(let i = 0; i < myLibrary.length; i++){
+            CreateCard(i)
+        }
+    }
+    else{
+        CreateCard();
+    }
+}
+
